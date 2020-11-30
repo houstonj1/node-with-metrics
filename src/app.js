7cc5d02c47f4f9b2401081c172c6210f37134087
James Houston <james-houston@pluralsight.com>
2020-11-29 22:35:48 -0500

    feat(app): Expose request duration metric

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
-const register = require('./metrics')
+const { httpRequestDurationMicroseconds, register } = require('./metrics')
 
 const VERSION = require('../package.json').version || 'none'
 
-app.get('/version', (_, res) => {
+app.get('/version', (req, res) => {
+    const end = httpRequestDurationMicroseconds.startTimer()
     res.status(200).json({
         version: VERSION
     })
+    end({ route: req.url, code: res.statusCode, method: req.method })
 })
 
-app.get('/metrics', (_, res) => {
+app.get('/metrics', (req, res) => {
+    const end = httpRequestDurationMicroseconds.startTimer()
     res.status(200).end(register.metrics())
+    end({ route: req.url, code: res.statusCode, method: req.method })
+})
+
+app.get('/', (req, res) => {
+    const end = httpRequestDurationMicroseconds.startTimer()
+    res.status(200).send('Yay metrics!')
+    end({ route: req.url, code: res.statusCode, method: req.method })
 })
 
 module.exports = app
--- src/metrics.js
+++ src/metrics.js
@@ -5,4 +5,16 @@ const labels = { application: 'node-with-metrics' }
 
 client.collectDefaultMetrics({ labels, register })
 
-module.exports = register
+const httpRequestDurationMicroseconds = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in microseconds',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+})
+
+register.registerMetric(httpRequestDurationMicroseconds)
+
+module.exports = {
+    httpRequestDurationMicroseconds,
+    register
+}
